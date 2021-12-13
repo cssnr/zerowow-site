cc480391063a4a2d6018ea51d1f07c5352786b28
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2021-12-13 00:39:28 -0800

    Dev (#2)
    Updates

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import GuildProfile, GuildNews, GuildApplicants
 
+admin.site.site_header = 'Zero Wow Administration'
+
 
 @admin.register(GuildProfile)
 class GuildProfileAdmin(admin.ModelAdmin):
