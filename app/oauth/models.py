7d1704b66cb1a631c7c773bc52e6b1ef4a24e581
Shane <shane@shanerice.me>
2021-12-15 21:13:49 -0800

    Adding migrations

--- app/oauth/models.py
+++ app/oauth/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     id = models.AutoField(primary_key=True)
-    avatar_hash = models.CharField(blank=True, max_length=32)
-    access_token = models.CharField(blank=True, max_length=32)
-    refresh_token = models.CharField(blank=True, max_length=32)
-    expires_in = models.DateTimeField(blank=True, null=True)
+    avatar_hash = models.CharField(null=True, blank=True, max_length=32)
+    access_token = models.CharField(null=True, blank=True, max_length=32)
+    refresh_token = models.CharField(null=True, blank=True, max_length=32)
+    expires_in = models.DateTimeField(null=True, blank=True)
 
     discord_roles = models.JSONField(blank=True, default=list)
     guild_member = models.BooleanField(blank=True, default=False)
